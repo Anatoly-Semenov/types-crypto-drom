e39719d411fd96ca7ffae4f92186b2a72fe09eb9
Anatoly Semenov <anatolysrmenov@gmail.com>
2022-11-11 14:25:51 +0300

    Add types to base file

--- src/base.ts
+++ src/base.ts
@@ -3,3 +3,20 @@
 export interface AnyObject {
 	[key: string]: any
 }
+
+export interface Meta {
+	page_size: number
+	total_items: number
+	total_pages: number
+	current_page: number
+}
+
+export interface ResponseList<T> {
+	data: T[]
+	meta: Meta
+}
+
+export enum SortOrder {
+	DESC = "DESC",
+	ASC = "ASC"
+}
